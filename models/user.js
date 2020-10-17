06293e38564a89875524b99e9436ccf066043827
lamkimphu258 <lamkimphu258@gmail.com>
2020-10-18 06:25:14 +0700

    Add unit test

--- models/user.js
+++ models/user.js
@@ -1,45 +1,52 @@
-const mongoose = require('mongoose')
-const Joi = require('joi')
+const mongoose = require("mongoose");
+const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
-const jwt = require('jsonwebtoken')
-const config = require('config')
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        min:5,
-        max:255,
-        required:true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        min: 8,
-        max: 26,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    min: 5,
+    max: 255,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    min: 8,
+    max: 26,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({_id:this._id}, config.get('jwtPrivateKey'))
-    return token
-}
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("jwtPrivateKey")
+  );
+  return token;
+};
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
 const validateUser = (user) => {
-    const schema = Joi.object({
-        name: Joi.string().min(5).max(255).required(),
-        email: Joi.string().required().email(),
-        password: passwordComplexity(),
-    })
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(255).required(),
+    email: Joi.string().required().email(),
+    password: passwordComplexity(),
+  });
 
-    return schema.validate(user)
-}
+  return schema.validate(user);
+};
 
-exports.User = User
-exports.validate = validateUser
+exports.User = User;
+exports.validate = validateUser;
